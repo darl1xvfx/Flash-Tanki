41187d01a91f0f6affee89ee1c4837585e0e6f16
drlxzar <142324585+ngrikd1@users.noreply.github.com>
2024-05-20 22:34:13 +0300

    full fix Flamethrower

--- src/main/kotlin/flashtanki/server/battles/weapons/FlamethrowerWeaponHandler.kt
+++ src/main/kotlin/flashtanki/server/battles/weapons/FlamethrowerWeaponHandler.kt
@@ -16,12 +16,12 @@ class FlamethrowerWeaponHandler(
 ) : WeaponHandler(player, weapon) {
   private val random = Random.Default
   private var fire = "0"
+  private var accumulatedDamage = mutableMapOf<String, Double>()
 
   suspend fun fireStart(startFire: StartFire) {
     val tank = player.tank ?: throw Exception("No Tank")
     val battle = player.battle
 
-    fire = "0.5"
 
     Command(CommandName.ClientStartFire, tank.id).send(battle.players.exclude(player).ready())
   }
@@ -29,40 +29,53 @@ class FlamethrowerWeaponHandler(
   suspend fun fireTarget(target: flashtanki.server.client.weapons.flamethrower.FireTarget) {
     val sourceTank = player.tank ?: throw Exception("No Tank")
     val battle = player.battle
-    val fires = 0.5
+    fire = "0.6"
 
-    if (fires > 0) {
-      val targetTanks = battle.players
-        .mapNotNull { player -> player.tank }
-        .filter { tank -> target.targets.contains(tank.id) }
-        .filter { tank -> tank.state == TankState.Active }
+    val targetTanks = battle.players
+      .mapNotNull { player -> player.tank }
+      .filter { tank -> target.targets.contains(tank.id) }
+      .filter { tank -> tank.state == TankState.Active }
 
-      targetTanks.forEach { targetTank ->
-        val damage = damageCalculator.calculate(sourceTank, targetTank)
-        val param1 = fire
+    targetTanks.forEach { targetTank ->
+      val randomDamage = random.nextInt(34, 76).toDouble()
+      val damage = damageCalculator.calculate(sourceTank, targetTank)
 
-        Command(CommandName.Temperature, targetTank.id, param1).sendTo(battle)
+      accumulatedDamage[targetTank.id] = (accumulatedDamage[targetTank.id] ?: 0.0) + randomDamage
 
-        battle.damageProcessor.dealDamage(sourceTank, targetTank, damage.damage, damage.isCritical)
-        if (fire == "0.5") {
-          delay(2000)
-          if (fire != "0.5") {
-            val param1Stop = "0"
-            Command(CommandName.Temperature, targetTank.id, param1Stop).sendTo(battle)
-          }
-        }
-      }
+      val param1 = fire
+      Command(CommandName.Temperature, targetTank.id, param1).sendTo(battle)
+
+      battle.damageProcessor.dealDamage(sourceTank, targetTank, damage.damage, damage.isCritical)
+
+      delay(2000)
     }
   }
 
   suspend fun fireStop(stopFire: StopFire) {
     val tank = player.tank ?: throw Exception("No Tank")
     val battle = player.battle
+    val targetTanks = battle.players.mapNotNull { it.tank }.filter { it.state == TankState.Active }
 
     fire = "0"
 
-    Command(CommandName.Temperature, tank.id, fire).sendTo(battle)
+    targetTanks.forEach { targetTank ->
+      Command(CommandName.ClientStopFire, tank.id).send(battle.players.exclude(player).ready())
 
-    Command(CommandName.ClientStopFire, tank.id).send(battle.players.exclude(player).ready())
+      if (targetTank.health > 1.0) {
+        val damage = damageCalculator.calculate(tank, targetTank)
+        while ((accumulatedDamage[targetTank.id] ?: 0.0) > 0 && targetTank.health > 1.0) {
+          val damageToDeal = damage.damage / 2.5
+          battle.damageProcessor.dealDamage(player.tank!!, targetTank, damageToDeal, isCritical = false)
+          accumulatedDamage[targetTank.id] = (accumulatedDamage[targetTank.id] ?: 0.0) - damageToDeal
+          val currentTemperature = ((accumulatedDamage[targetTank.id] ?: 0.0)).coerceIn(0.0000, 0.5)
+          Command(CommandName.Temperature, targetTank.id, currentTemperature.toString()).sendTo(battle)
+
+          delay(2000)
+        }
+      } else {
+        Command(CommandName.Temperature, targetTank.id, "0.0").sendTo(battle)
+      }
+      Command(CommandName.Temperature, targetTank.id, fire).sendTo(battle)
+    }
   }
 }
