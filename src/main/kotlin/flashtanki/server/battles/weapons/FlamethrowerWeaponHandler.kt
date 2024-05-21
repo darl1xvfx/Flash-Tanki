3dabd94d39f227a8e72626bd24ff1c3c5e6d1fe4
drlxzar <142324585+ngrikd1@users.noreply.github.com>
2024-05-21 16:40:19 +0300

    Now it's definitely a full-fixed flamethrower.

--- src/main/kotlin/flashtanki/server/battles/weapons/FlamethrowerWeaponHandler.kt
+++ src/main/kotlin/flashtanki/server/battles/weapons/FlamethrowerWeaponHandler.kt
@@ -1,6 +1,7 @@
 package flashtanki.server.battles.weapons
 
 import flashtanki.server.battles.*
+import flashtanki.server.battles.mode.DeathmatchModeHandler
 import flashtanki.server.client.send
 import flashtanki.server.client.weapons.flamethrower.StartFire
 import flashtanki.server.client.weapons.flamethrower.StopFire
@@ -17,7 +18,6 @@ class FlamethrowerWeaponHandler(
   private val random = Random.Default
   private var fire = "0"
   private var accumulatedDamage = mutableMapOf<String, Double>()
-
 
   suspend fun fireStart(startFire: StartFire) {
     val tank = player.tank ?: throw Exception("No Tank")
@@ -37,16 +37,20 @@ class FlamethrowerWeaponHandler(
       .filter { tank -> tank.state == TankState.Active }
 
     targetTanks.forEach { targetTank ->
-      val damage = damageCalculator.calculate(sourceTank, targetTank)
-      val damageToDeal = damage.damage / random.nextInt(2, 4)
+      if (sourceTank != targetTank && (battle.properties[BattleProperty.FriendlyFireEnabled] ||
+                battle.modeHandler is DeathmatchModeHandler || player.team != targetTank.player.team)) {
 
-      accumulatedDamage[targetTank.id] = (accumulatedDamage[targetTank.id] ?: 0.0) + damageToDeal
+        val damage = damageCalculator.calculate(sourceTank, targetTank)
+        val damageToDeal = damage.damage / random.nextInt(2, 4)
 
-      val param1 = fire
-      Command(CommandName.Temperature, targetTank.id, param1).sendTo(battle)
+        accumulatedDamage[targetTank.id] = (accumulatedDamage[targetTank.id] ?: 0.0) + damageToDeal
 
-      battle.damageProcessor.dealDamage(sourceTank, targetTank, damage.damage, damage.isCritical)
-      delay(2000)
+        val param1 = fire
+        Command(CommandName.Temperature, targetTank.id, param1).sendTo(battle)
+
+        battle.damageProcessor.dealDamage(sourceTank, targetTank, damage.damage, damage.isCritical)
+        delay(2000)
+      }
     }
   }
 
@@ -57,22 +61,25 @@ class FlamethrowerWeaponHandler(
 
     fire = "0"
 
+    Command(CommandName.ClientStopFire, tank.id).send(battle.players.exclude(player).ready())
     targetTanks.forEach { targetTank ->
-      Command(CommandName.ClientStopFire, tank.id).send(battle.players.exclude(player).ready())
-      while ((accumulatedDamage[targetTank.id] ?: 0.0) > 0) {
-        val damage = damageCalculator.calculate(tank, targetTank)
-        val damageToDeal = damage.damage / random.nextInt(2, 4)
-        battle.damageProcessor.dealDamage(player.tank!!, targetTank, damageToDeal, isCritical = false)
-        accumulatedDamage[targetTank.id] = (accumulatedDamage[targetTank.id] ?: 0.0) - damageToDeal
-        val currentTemperature = ((accumulatedDamage[targetTank.id] ?: 0.0)).coerceIn(0.0, 0.5)
-        Command(CommandName.Temperature, targetTank.id, currentTemperature.toString()).sendTo(battle)
-        if (targetTank.health < 2.0) {
-          Command(CommandName.Temperature, targetTank.id, fire).sendTo(battle)
-          break
+      if (tank != targetTank && (battle.properties[BattleProperty.FriendlyFireEnabled] ||
+                battle.modeHandler is DeathmatchModeHandler || player.team != targetTank.player.team)) {
+        while ((accumulatedDamage[targetTank.id] ?: 0.0) > 0) {
+          val damage = damageCalculator.calculate(tank, targetTank)
+          val damageToDeal = damage.damage / random.nextInt(2, 4)
+          battle.damageProcessor.dealDamage(player.tank!!, targetTank, damageToDeal, isCritical = false)
+          accumulatedDamage[targetTank.id] = (accumulatedDamage[targetTank.id] ?: 0.0) - damageToDeal
+          val currentTemperature = (accumulatedDamage[targetTank.id] ?: 0.0).coerceIn(0.0, 0.5)
+          Command(CommandName.Temperature, targetTank.id, currentTemperature.toString()).sendTo(battle)
+          if (targetTank.health < 2.0) {
+            Command(CommandName.Temperature, targetTank.id, fire).sendTo(battle)
+            break
+          }
+          delay(1500)
         }
-        delay(1500)
+        Command(CommandName.Temperature, targetTank.id, fire).sendTo(battle)
       }
-      Command(CommandName.Temperature, targetTank.id, fire).sendTo(battle)
     }
   }
 }
