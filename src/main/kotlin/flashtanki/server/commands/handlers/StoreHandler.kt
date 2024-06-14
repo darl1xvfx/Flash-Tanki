59e1811357f3b33840f76c2512bf61c8900b5049
Bodya <Vavrencukbogdan28@gmail.com>
2024-06-14 23:47:10 +0300

    Update StoreHandler.kt

--- src/main/kotlin/flashtanki/server/commands/handlers/StoreHandler.kt
+++ src/main/kotlin/flashtanki/server/commands/handlers/StoreHandler.kt
@@ -19,96 +19,97 @@ class StoreHandler : ICommandHandler, KoinComponent {
   private val storeRegistry: IStoreRegistry by inject()
   private val storeItemConverter: IStoreItemConverter by inject()
   private val promoCodeService: IPromoCodeService by inject()
-  private val userRepository by inject<IUserRepository>()
+  private val userRepository: IUserRepository by inject()
+
+  private fun checkUser(socket: UserSocket): User {
+    return socket.user ?: throw Exception("No User")
+  }
 
   @CommandHandler(CommandName.OpenStore)
   suspend fun openStore(socket: UserSocket) {
+    val categories = storeRegistry.categories.values
+    val clientCategories = categories.map(storeItemConverter::toClientCategory)
+    val clientItems = categories.flatMap { it.items }.map(storeItemConverter::toClientItem)
+
     Command(
       CommandName.ClientOpenStore,
       OpenStoreWrapperData(
         data = OpenStoreData(
-          categories = storeRegistry.categories.values.map(storeItemConverter::toClientCategory),
-          items = storeRegistry.categories.values
-            .map { category -> category.items }
-            .flatten()
-            .map(storeItemConverter::toClientItem)
+          categories = clientCategories,
+          items = clientItems
         ).toJson()
       ).toJson()
     ).send(socket)
   }
 
-    @CommandHandler(CommandName.TryActivatePromocode)
-    suspend fun tryActivatePromocode(socket: UserSocket, promocode: String) {
-        val user = socket.user ?: throw Exception("No User")
-        if (promoCodeService.checkPromoCode(promocode)) {
-		    val promoPrize = 5000
-            Command(CommandName.ActivatePromocodeSuccessfully).send(socket)
-            user.crystals += promoPrize
+  @CommandHandler(CommandName.TryActivatePromocode)
+  suspend fun tryActivatePromocode(socket: UserSocket, promocode: String) {
+    val user = checkUser(socket)
+    if (promoCodeService.checkPromoCode(promocode)) {
+      val promoPrize = 5000
+      Command(CommandName.ActivatePromocodeSuccessfully).send(socket)
+      user.crystals += promoPrize
 
-            socket.updateCrystals()
-			Command(
-              CommandName.StorePaymentSuccess,
-              promoPrize.toString(),
-              0.toString(),
-              0.toString(),
-              when(socket.locale) {
-                SocketLocale.Russian -> 124221
-                SocketLocale.English -> 123444
-                SocketLocale.Portuguese -> 143111
-                else -> throw IllegalArgumentException("Unsupported locale: ${socket.locale}")
-              }.toString()
-            ).send(socket)
-            promoCodeService.removePromoCode(promocode)
-            logger.debug { "Player ${user.username} activated promocode ${promocode}" }
-        } else {
-            Command(CommandName.ActivatePromocodeFailed).send(socket)
-            logger.debug { "Player ${user.username} can't be activated promocode ${promocode}" }
-        }
-
+      socket.updateCrystals()
+      Command(
+        CommandName.StorePaymentSuccess,
+        promoPrize.toString(),
+        0.toString(),
+        0.toString(),
+        getLocaleValue(socket.locale)
+      ).send(socket)
+      promoCodeService.removePromoCode(promocode)
+      logger.debug { "Player ${user.username} activated promocode $promocode" }
+    } else {
+      Command(CommandName.ActivatePromocodeFailed).send(socket)
+      logger.debug { "Player ${user.username} failed to activate promocode $promocode" }
     }
+  }
 
   @CommandHandler(CommandName.StoreTryBuyItem)
   suspend fun storeTryBuyItem(socket: UserSocket, itemId: String, paymentMethodId: String) {
-    val user = socket.user ?: throw Exception("No User")
-    val paymentMethod = StorePaymentMethod.get(paymentMethodId) ?: throw IllegalArgumentException("Unknown payment method: $paymentMethodId")
+    val user = checkUser(socket)
+    val paymentMethod = StorePaymentMethod.get(paymentMethodId)
+      ?: throw IllegalArgumentException("Unknown payment method: $paymentMethodId")
 
     val item = storeRegistry.categories.values
-      .map { category -> category.items }
-      .flatten()
-      .single { item -> item.id == itemId }
+      .flatMap { it.items }
+      .single { it.id == itemId }
 
     Command(
       CommandName.StorePaymentSuccess,
-      (if (item.crystals != null) item.crystals.base else 0).toString(),
-      (if (item.crystals != null) item.crystals.bonus else 0).toString(),
+      (item.crystals?.base ?: 0).toString(),
+      (item.crystals?.bonus ?: 0).toString(),
       0.toString(),
-      when (socket.locale) {
-        SocketLocale.Russian -> 124221
-        SocketLocale.English -> 123444
-        SocketLocale.Portuguese -> 143111
-        else -> throw IllegalArgumentException("Unsupported locale: ${socket.locale}")
-      }.toString()
+      getLocaleValue(socket.locale)
     ).send(socket)
 
-    if (item.crystals != null) {
-      user.crystals += item.crystals.base + item.crystals.bonus
+    item.crystals?.let {
+      user.crystals += it.base + it.bonus
       socket.updateCrystals()
-      logger.debug { "Player ${user.username} has crystals added (${item.crystals.base} + (${item.crystals.bonus})" }
-
+      logger.debug { "Player ${user.username} added crystals (${it.base} + ${it.bonus})" }
     }
 
-    if (item.premium != null) {
-      socket.addPremiumAccount(item.premium.base)
+    item.premium?.let {
+      socket.addPremiumAccount(it.base)
       userRepository.updateUser(user)
-      logger.debug { "Player ${user.username} has premium days added (${item.premium.base})" }
+      logger.debug { "Player ${user.username} added premium days (${it.base})" }
     }
 
     if (itemId == "clan_license") {
       Command(CommandName.AddClanLicense).send(socket)
-      logger.debug { "Player ${user.username} has been added clan_license" }
+      logger.debug { "Player ${user.username} added clan_license" }
     }
 
-    logger.debug { "Player ${user.username} bought ${item.id} (payment method: $paymentMethod)" }
+    logger.debug { "Player ${user.username} bought ${item.id} with payment method: $paymentMethod" }
   }
 
+  private fun getLocaleValue(locale: SocketLocale?): String {
+    return when (locale) {
+      SocketLocale.Russian -> "124221"
+      SocketLocale.English -> "123444"
+      SocketLocale.Portuguese -> "143111"
+      else -> throw IllegalArgumentException("Unsupported or null locale: $locale")
+    }
+  }
 }
