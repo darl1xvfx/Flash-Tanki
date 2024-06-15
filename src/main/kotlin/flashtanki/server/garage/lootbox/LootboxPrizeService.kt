5831e9f192d670a4d386e758e339402f0d49a638
Bodya <Vavrencukbogdan28@gmail.com>
2024-06-15 23:47:39 +0300

    Update LootboxPrizeService.kt

--- src/main/kotlin/flashtanki/server/garage/lootbox/LootboxPrizeService.kt
+++ src/main/kotlin/flashtanki/server/garage/lootbox/LootboxPrizeService.kt
@@ -1,31 +1,32 @@
 package flashtanki.server.garage.lootbox
 
 import com.squareup.moshi.Json
+import kotlin.random.Random
 
 class LootboxPrizeService {
     private val rewardsMap = mapOf(
-        "17,9" to "Пакет 3500 кристаллов",
-        "12,2" to "Набор 125 двойного урона",
-        "11,6" to "Набор 125 повышенной защиты",
-        "10,9" to "Набор 125 ускорений",
-        "10,7" to "Набор 125 мин",
-        "8,7" to "Пакет 10 000 кристаллов",
-        "5,3" to "Набор 125 ремкомплектов",
-        "4,2" to "Комплект 100 всех припасов",
-        "2,9" to "краска с фиолетового",
-        "2,8" to "Набор 5 золотых ящиков",
-        "2,1" to "Пакет 25 000 кристаллов",
-        "1,2" to "3 дня премиум аккаунта",
-        "1,1" to "Набор 10 золотых ящиков",
-        "1" to "Комплект 250 всех припасов",
-        "0,6" to "краска с голд.свечения",
-        "0,6" to "Пакет 100 000 кристаллов",
-        "0,4" to "10 дней премиум аккаунта",
-        "0,1" to "Пакет 300 000 кристаллов",
-        "0,07" to "ХТ"
+        "17,9" to Pair("Пакет 3500 кристаллов", "COMMON"),
+        "12,2" to Pair("Набор 125 двойного урона", "COMMON"),
+        "11,6" to Pair("Набор 125 повышенной защиты", "COMMON"),
+        "10,9" to Pair("Набор 125 ускорений", "COMMON"),
+        "10,7" to Pair("Набор 125 мин", "COMMON"),
+        "8,7" to Pair("Пакет 10 000 кристаллов", "UNCOMMON"),
+        "5,3" to Pair("Набор 125 ремкомплектов", "UNCOMMON"),
+        "4,2" to Pair("Комплект 100 всех припасов", "UNCOMMON"),
+        "2,9" to Pair("краска с фиолетового", "RARE"),
+        "2,8" to Pair("Набор 5 золотых ящиков", "RARE"),
+        "2,1" to Pair("Пакет 25 000 кристаллов", "RARE"),
+        "1,2" to Pair("3 дня премиум аккаунта", "EPIC"),
+        "1,1" to Pair("Набор 10 золотых ящиков", "EPIC"),
+        "1" to Pair("Комплект 250 всех припасов", "EPIC"),
+        "0,6" to Pair("краска с голд.свечения", "LEGENDARY"),
+        "0,6" to Pair("Пакет 100 000 кристаллов", "LEGENDARY"),
+        "0,4" to Pair("10 дней премиум аккаунта", "LEGENDARY"),
+        "0,1" to Pair("Пакет 300 000 кристаллов", "EXOTIC"),
+        "0,07" to Pair("ХТ", "EXOTIC")
     )
 
-    private val names = rewardsMap.values.toList()
+    private val namesAndRarities = rewardsMap.values.toList()
 
     private val previews = listOf(
         978053, 153186, 504645, 716565, 71622, 824172, 209092, 629496, 730749, 882375, 542698, 826132,
@@ -33,14 +34,17 @@ class LootboxPrizeService {
     )
 
     suspend fun getRandomReward(count: Int): List<LootboxPrize> {
-        require(count <= names.size && count <= previews.size) { "Requested count exceeds available elements." }
+        require(count <= namesAndRarities.size && count <= previews.size) { "Requested count exceeds available elements." }
+
+        val shuffledRewards = namesAndRarities.shuffled()
+        val shuffledPreviews = previews.shuffled()
 
         return List(count) { i ->
             LootboxPrize(
-                category = "COMMON",
+                category = shuffledRewards[i].second,
                 count = 1,
-                preview = previews[i],
-                name = names[i]
+                preview = shuffledPreviews[i],
+                name = shuffledRewards[i].first
             )
         }
     }
