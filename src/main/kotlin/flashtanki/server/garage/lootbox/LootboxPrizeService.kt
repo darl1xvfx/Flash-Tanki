16eb2a2576b57135c1532a8bb37e9d35ba27b5f9
TitanoMachina <153441107+KAPJICON1337@users.noreply.github.com>
2024-06-16 12:02:32 +0500

    Update LootboxPrizeService.kt

--- src/main/kotlin/flashtanki/server/garage/lootbox/LootboxPrizeService.kt
+++ src/main/kotlin/flashtanki/server/garage/lootbox/LootboxPrizeService.kt
@@ -6,48 +6,73 @@ import kotlin.random.Random
 data class Prize(
     val name: String,
     val rarity: String,
-    val preview: Int
+    val preview: Int,
+    val id: String
 )
 
 class LootboxPrizeService {
     private val prizes = listOf(
-        Prize("Пакет 3500 кристаллов", "COMMON", 978053),
-        Prize("Набор 125 двойного урона", "COMMON", 153186),
-        Prize("Набор 125 повышенной защиты", "COMMON", 504645),
-        Prize("Набор 125 ускорений", "COMMON", 716565),
-        Prize("Набор 125 мин", "COMMON", 71622),
-        Prize("Пакет 10 000 кристаллов", "UNCOMMON", 824172),
-        Prize("Набор 125 ремкомплектов", "UNCOMMON", 209092),
-        Prize("Комплект 100 всех припасов", "UNCOMMON", 629496),
-        Prize("краска с фиолетового", "EPIC", 730749),
-        Prize("Набор 5 золотых ящиков", "UNCOMMON", 882375),
-        Prize("Пакет 25 000 кристаллов", "RARE", 542698),
-        Prize("3 дня премиум аккаунта", "UNCOMMON", 826132),
-        Prize("Набор 10 золотых ящиков", "RARE", 468704),
-        Prize("Комплект 250 всех припасов", "RARE", 254675),
-        Prize("краска с голд.свечения", "LEGENDARY", 350240),
-        Prize("Пакет 100 000 кристаллов", "LEGENDARY", 978053),
-        Prize("10 дней премиум аккаунта", "RARE", 153186),
-        Prize("Пакет 300 000 кристаллов", "EXOTIC", 504645),
-        Prize("ХТ", "EXOTIC", 716565)
+        Prize("Пакет 3500 кристаллов", "COMMON", 978053, "crystals_3500"),
+        Prize("Набор 125 двойного урона", "COMMON", 153186, "double_damage_125"),
+        Prize("Набор 125 повышенной защиты", "COMMON", 504645, "double_armor_125"),
+        Prize("Набор 125 ускорений", "COMMON", 716565, "nitro_125"),
+        Prize("Набор 125 мин", "COMMON", 71622, "mine_125"),
+        Prize("Пакет 10 000 кристаллов", "UNCOMMON", 824172, "crystals_10000"),
+        Prize("Набор 125 ремкомплектов", "UNCOMMON", 209092, "health_125"),
+        Prize("Комплект 100 всех припасов", "UNCOMMON", 629496, "allsupplies_100"),
+        Prize("краска с фиолетового", "EPIC", 730749, "epic_paint"),
+        Prize("Набор 5 золотых ящиков", "UNCOMMON", 882375, "goldboxes_5"),
+        Prize("Пакет 25 000 кристаллов", "RARE", 542698, "crystals_25000"),
+        Prize("3 дня премиум аккаунта", "UNCOMMON", 826132, "premiumdays_3"),
+        Prize("Набор 10 золотых ящиков", "RARE", 468704, "goldboxes_10"),
+        Prize("Комплект 250 всех припасов", "RARE", 254675, "allsupplies_250"),
+        Prize("краска с голд.свечения", "LEGENDARY", 350240, "legendary_paint"),
+        Prize("Пакет 100 000 кристаллов", "EPIC", 978053, "crystals_100000"),
+        Prize("10 дней премиум аккаунта", "RARE", 153186, "premiumdays_10"),
+        Prize("Пакет 300 000 кристаллов", "LEGENDARY", 504645, "crystals_300000"),
+        Prize("ХТ", "EXOTIC", 716565, "xt"),
+        Prize("Пакет 1 000 000 кристаллов", "EXOTIC", 1500009, "crystals_1000000")
     )
 
     private val probabilities = mapOf(
-        "COMMON" to /*0.35*/0.50,
-        "UNCOMMON" to /*0.25*/0.30,
-        "RARE" to /*0.17*/0.14,
-        "EPIC" to /*0.15*/0.10,
+        "COMMON" to 0.50,
+        "UNCOMMON" to 0.30,
+        "RARE" to 0.14,
+        "EPIC" to 0.10,
         "LEGENDARY" to 0.08,
         "EXOTIC" to 0.08
     )
 
-    val customOrder = mapOf(
+    val categoryOrder = mapOf(
         "COMMON" to 1,
         "UNCOMMON" to 2,
         "RARE" to 3,
         "EPIC" to 4,
         "LEGENDARY" to 5,
         "EXOTIC" to 6
+    )
+
+    val prizeOrder = mapOf(
+        "crystals_3500" to 1,
+        "double_damage_125" to 2,
+        "double_armor_125" to 3,
+        "nitro_125" to 4,
+        "mine_125" to 5,
+        "crystals_10000" to 7,
+        "health_125" to 8,
+        "allsupplies_100" to 9,
+        "goldboxes_5" to 10,
+        "premiumdays_3" to 11,
+        "crystals_25000" to 12,
+        "goldboxes_10" to 13,
+        "allsupplies_250" to 14,
+        "premiumdays_10" to 15,
+        "epic_paint" to 16,
+        "crystals_100000" to 17,
+        "legendary_paint" to 18,
+        "crystals_300000" to 19,
+        "xt" to 20,
+        "crystals_1000000" to 21
     )
 
     suspend fun getRandomReward(count: Int): List<LootboxPrize> {
@@ -65,9 +90,10 @@ class LootboxPrizeService {
                 category = prize.rarity,
                 count = 1,
                 preview = prize.preview,
-                name = prize.name
+                name = prize.name,
+                id = prize.id
             )
-        }.sortedWith(compareBy { customOrder[it.category] })
+        }.sortedWith(compareBy { categoryOrder[it.category] }).sortedWith(compareBy { prizeOrder[it.id] })
     }
 
     private fun selectRarity(): String {
@@ -87,5 +113,6 @@ data class LootboxPrize(
     @Json(name = "category") val category: String,
     @Json(name = "count") val count: Int,
     @Json(name = "preview") val preview: Int,
-    @Json(name = "name") val name: String
+    @Json(name = "name") val name: String,
+    val id: String
 )
