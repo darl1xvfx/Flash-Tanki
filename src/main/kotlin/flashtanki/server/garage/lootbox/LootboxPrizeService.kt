e05dfd3cdedfde4f70b28a36c8b2e72743d5c09c
Bodya <Vavrencukbogdan28@gmail.com>
2024-06-16 14:18:45 +0300

    add

--- src/main/kotlin/flashtanki/server/garage/lootbox/LootboxPrizeService.kt
+++ src/main/kotlin/flashtanki/server/garage/lootbox/LootboxPrizeService.kt
@@ -12,26 +12,26 @@ data class Prize(
 
 class LootboxPrizeService {
     private val prizes = listOf(
-        Prize("Пакет 3500 кристаллов", "COMMON", 978053, "crystals_3500"),
-        Prize("Набор 125 двойного урона", "COMMON", 153186, "double_damage_125"),
-        Prize("Набор 125 повышенной защиты", "COMMON", 504645, "double_armor_125"),
-        Prize("Набор 125 ускорений", "COMMON", 716565, "nitro_125"),
-        Prize("Набор 125 мин", "COMMON", 71622, "mine_125"),
-        Prize("Пакет 10 000 кристаллов", "UNCOMMON", 824172, "crystals_10000"),
-        Prize("Набор 125 ремкомплектов", "UNCOMMON", 209092, "health_125"),
-        Prize("Комплект 100 всех припасов", "UNCOMMON", 629496, "allsupplies_100"),
-        Prize("краска с фиолетового", "EPIC", 730749, "epic_paint"),
-        Prize("Набор 5 золотых ящиков", "UNCOMMON", 882375, "goldboxes_5"),
-        Prize("Пакет 25 000 кристаллов", "RARE", 542698, "crystals_25000"),
-        Prize("3 дня премиум аккаунта", "UNCOMMON", 826132, "premiumdays_3"),
-        Prize("Набор 10 золотых ящиков", "RARE", 468704, "goldboxes_10"),
-        Prize("Комплект 250 всех припасов", "RARE", 254675, "allsupplies_250"),
-        Prize("краска с голд.свечения", "LEGENDARY", 350240, "legendary_paint"),
-        Prize("Пакет 100 000 кристаллов", "EPIC", 978053, "crystals_100000"),
-        Prize("10 дней премиум аккаунта", "RARE", 153186, "premiumdays_10"),
-        Prize("Пакет 300 000 кристаллов", "LEGENDARY", 504645, "crystals_300000"),
-        Prize("ХТ", "EXOTIC", 716565, "xt"),
-        Prize("Пакет 1 000 000 кристаллов", "EXOTIC", 1500009, "crystals_1000000")
+        Prize("Пакет 3500 кристаллов", "COMMON", 978053, "crystals_3500"), //1
+        Prize("Набор 125 двойного урона", "COMMON", 153186, "double_damage_125"), //2
+        Prize("Набор 125 повышенной защиты", "COMMON", 504645, "double_armor_125"), //3
+        Prize("Набор 125 ускорений", "COMMON", 716565, "nitro_125"), //4
+        Prize("Набор 125 мин", "COMMON", 71622, "mine_125"), //5
+        Prize("Пакет 10 000 кристаллов", "UNCOMMON", 824172, "crystals_10000"), //6
+        Prize("Набор 125 ремкомплектов", "UNCOMMON", 209092, "health_125"), //7
+        Prize("Комплект 100 всех припасов", "UNCOMMON", 629496, "allsupplies_100"), //8
+        Prize("краска с фиолетового", "EPIC", 730749, "epic_paint"), //9
+        Prize("Набор 5 золотых ящиков", "UNCOMMON", 882375, "goldboxes_5"), //9
+        Prize("Пакет 25 000 кристаллов", "RARE", 542698, "crystals_25000"), //10
+        Prize("3 дня премиум аккаунта", "UNCOMMON", 826132, "premiumdays_3"), //11
+        Prize("Набор 10 золотых ящиков", "RARE", 468704, "goldboxes_10"), //12
+        Prize("Комплект 250 всех припасов", "RARE", 254675, "allsupplies_250"), //13
+        Prize("краска с голд.свечения", "LEGENDARY", 350240, "legendary_paint"), //14
+        Prize("Пакет 100 000 кристаллов", "EPIC", 978053, "crystals_100000"), //15
+        Prize("10 дней премиум аккаунта", "RARE", 153186, "premiumdays_10"), //16
+        Prize("Пакет 300 000 кристаллов", "LEGENDARY", 504645, "crystals_300000"), //17
+        Prize("ХТ", "EXOTIC", 716565, "xt"), //18
+        Prize("Пакет 1 000 000 кристаллов", "EXOTIC", 1500009, "crystals_1000000") //19
     )
 
     private val probabilities = mapOf(
