5f8521f5f4e28742782ffbe8a0992ea5d3142e6c
Bodya <Vavrencukbogdan28@gmail.com>
2024-06-30 01:21:38 +0300

    Update OAuthService.kt

--- src/main/kotlin/flashtanki/server/discord/OAuthService.kt
+++ src/main/kotlin/flashtanki/server/discord/OAuthService.kt
@@ -1,36 +1,39 @@
 package flashtanki.server.bot.discord
 
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.request.*
+import io.ktor.client.engine.cio.*
 import io.ktor.http.*
 import io.ktor.server.engine.*
-import io.ktor.client.engine.cio.*
 import io.ktor.server.netty.*
 import io.ktor.server.application.*
-import io.ktor.server.sessions.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import mu.KotlinLogging
+import io.ktor.server.sessions.*
 import io.ktor.server.auth.*
+import mu.KotlinLogging
 import org.koin.core.component.KoinComponent
 
-class OAuthService() : KoinComponent {
-    private val logger = KotlinLogging.logger { }
+class OAuthService : KoinComponent {
+    private val logger = KotlinLogging.logger {}
     private val httpClient = HttpClient(CIO)
 
-    fun init()
-    {
+    fun init() {
         embeddedServer(Netty, port = 7777) {
             install(Authentication) {
                 oauth("discord-oauth") {
                     client = httpClient
-                    providerLookup = { OAuthServerSettings.OAuth2ServerSettings(
-                        name = "discord",
-                        authorizeUrl = "https://discord.com/api/oauth2/authorize",
-                        accessTokenUrl = "https://discord.com/api/oauth2/token",
-                        clientId = "1248976181466890271",
-                        clientSecret = "uMgbC3CEdO2iVAKm7jLBvgLX3TRzBfrv",
-                        defaultScopes = listOf("identify")
-                    ) }
+                    providerLookup = {
+                        OAuthServerSettings.OAuth2ServerSettings(
+                            name = "discord",
+                            authorizeUrl = "https://discord.com/api/oauth2/authorize",
+                            accessTokenUrl = "https://discord.com/api/oauth2/token",
+                            clientId = "1248976181466890271",
+                            clientSecret = "uMgbC3CEdO2iVAKm7jLBvgLX3TRzBfrv",
+                            defaultScopes = listOf("identify")
+                        )
+                    }
                     urlProvider = { "http://localhost:7777/oauth/discord/redirect" }
                 }
             }
@@ -39,19 +42,25 @@ class OAuthService() : KoinComponent {
                 authenticate("discord-oauth") {
                     get("/oauth/discord/redirect") {
                         val principal = call.authentication.principal<OAuthAccessTokenResponse.OAuth2>()
+                        if (principal != null) {
+                            call.respondText("Authentication successful")
+                        } else {
+                            call.respondText("Authentication failed")
+                        }
                     }
                 }
             }
         }.start()
-        logger.debug { "OAuthService inited!" }
+        logger.debug { "OAuthService initialized!" }
     }
 
     suspend fun getUserInfo(accessToken: String): String {
-        val resp = httpClient.get("https://discord.com/api/users/@me") {
+        val response = httpClient.get("https://discord.com/api/users/@me") {
             headers {
+                append(HttpHeaders.ContentType, ContentType.Application.FormUrlEncoded.toString())
                 append(HttpHeaders.Authorization, "Bearer $accessToken")
             }
         }
-        return resp.toString()
+        return response.body<String>()
     }
 }
