2a27ee7ffcb73bf518e2aec841026dbc417923da
Bodya <Vavrencukbogdan28@gmail.com>
2024-07-01 01:10:05 +0300

    Update OAuthService.kt

--- src/main/kotlin/flashtanki/server/discord/OAuthService.kt
+++ src/main/kotlin/flashtanki/server/discord/OAuthService.kt
@@ -1,39 +1,36 @@
 package flashtanki.server.bot.discord
 
 import io.ktor.client.*
-import io.ktor.client.call.*
 import io.ktor.client.request.*
-import io.ktor.client.engine.cio.*
 import io.ktor.http.*
 import io.ktor.server.engine.*
+import io.ktor.client.engine.cio.*
 import io.ktor.server.netty.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
-import io.ktor.server.auth.*
+import io.ktor.server.routing.*
 import mu.KotlinLogging
+import io.ktor.server.auth.*
 import org.koin.core.component.KoinComponent
 
-class OAuthService : KoinComponent {
-    private val logger = KotlinLogging.logger {}
+class OAuthService() : KoinComponent {
+    private val logger = KotlinLogging.logger { }
     private val httpClient = HttpClient(CIO)
 
-    fun init() {
+    fun init()
+    {
         embeddedServer(Netty, port = 7777) {
             install(Authentication) {
                 oauth("discord-oauth") {
                     client = httpClient
-                    providerLookup = {
-                        OAuthServerSettings.OAuth2ServerSettings(
-                            name = "discord",
-                            authorizeUrl = "https://discord.com/api/oauth2/authorize",
-                            accessTokenUrl = "https://discord.com/api/oauth2/token",
-                            clientId = "1249282768786030653",
-                            clientSecret = "n_MDYqC6v7mhkhk7U2DQCOVqNoFxX4Qv",
-                            defaultScopes = listOf("identify")
-                        )
-                    }
+                    providerLookup = { OAuthServerSettings.OAuth2ServerSettings(
+                        name = "discord",
+                        authorizeUrl = "https://discord.com/api/oauth2/authorize",
+                        accessTokenUrl = "https://discord.com/api/oauth2/token",
+                        clientId = "1249282768786030653",
+                        clientSecret = "n_MDYqC6v7mhkhk7U2DQCOVqNoFxX4Qv",
+                        defaultScopes = listOf("identify")
+                    ) }
                     urlProvider = { "http://localhost:7777/api/oauth/discord/redirect" }
                 }
             }
@@ -42,35 +39,19 @@ class OAuthService : KoinComponent {
                 authenticate("discord-oauth") {
                     get("/oauth/discord/redirect") {
                         val principal = call.authentication.principal<OAuthAccessTokenResponse.OAuth2>()
-                        if (principal != null) {
-                            call.respondText("Authentication successful")
-                        } else {
-                            call.respondText("Authentication failed")
-                        }
                     }
-                }
-
-                get("/start") {
-                    val authorizationUrl = "https://discord.com/oauth2/authorize?" +
-                            "client_id=1249282768786030653" +
-                            "&response_type=code" +
-                            "&redirect_uri=http%3A%2F%2Flocalhost%3A7777%2Fapi%2Foauth%2Fdiscord%2Fredirect" +
-                            "&scope=identify+gdm.join+guilds+email+connections+guilds.join"
-
-                    call.respondRedirect(authorizationUrl)
                 }
             }
         }.start()
-        logger.debug { "OAuthService initialized!" }
+        logger.debug { "OAuthService inited!" }
     }
 
     suspend fun getUserInfo(accessToken: String): String {
-        val response = httpClient.get("https://discord.com/api/users/@me") {
+        val resp = httpClient.get("https://discord.com/api/users/@me") {
             headers {
                 append(HttpHeaders.Authorization, "Bearer $accessToken")
-                append(HttpHeaders.ContentType, ContentType.Application.FormUrlEncoded.toString())
             }
         }
-        return response.body<String>()
+        return resp.toString()
     }
 }
