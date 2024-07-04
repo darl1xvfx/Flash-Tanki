228f09538173cde35acfb9c14247fb0137226685
Bodya <Vavrencukbogdan28@gmail.com>
2024-07-04 13:23:29 +0300

    Update autoResponses Handlers.kt

--- src/main/kotlin/flashtanki/server/discord/autoResponses Handlers.kt
+++ src/main/kotlin/flashtanki/server/discord/autoResponses Handlers.kt
@@ -5,12 +5,18 @@ import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import org.koin.core.component.KoinComponent
 
-
 class autoResponsesHandlers : KoinComponent {
 
     suspend fun handleCommand(event: GuildMessageReceivedEvent) {
-        val (message, channel) = Triple(event.message.contentRaw, event.channel, event.author.id)
+        val message = event.message.contentRaw
+        val channel = event.channel
+        val authorId = event.author.id
 
+        if (message.startsWith("Ссылка") || message.startsWith("Link")) {
+            GlobalScope.launch {
+                channel.sendMessage("https://discord.gg/tjskEJ7SFb").queue()
+            }
+        }
         /*when {
             message.startsWith(" ") -> {
                 GlobalScope.launch {
@@ -18,24 +24,8 @@ class autoResponsesHandlers : KoinComponent {
                     channel.sendMessage(" ").queue()
                 }
             }
-            message.startsWith(" ") -> {
-                GlobalScope.launch {
-                    channel.sendMessage(" ").queue()
-                    channel.sendMessage(" ").queue()
-                }
-            }
-            message.startsWith(" ") -> {
-                GlobalScope.launch {
-                    channel.sendMessage(" ").queue()
-                    channel.sendMessage(" ").queue()
-                }
-            }
-            message.startsWith(" ") -> {
-                GlobalScope.launch {
-                    channel.sendMessage("").queue()
-                    channel.sendMessage(" ").queue()
-                }
-            }
-        }*/
+
+        }
+         */
     }
 }
